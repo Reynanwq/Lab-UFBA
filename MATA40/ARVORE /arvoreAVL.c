c809d9b812179091488daded8c54d6044a9fa7dc
Reynan Paiva <reynanwq@gmail.com>
2022-11-14 20:02:37 -0300

    [add] comentarios

---
+++ MATA40/ARVORE /arvoreAVL.c
@@ -0,0 +1,14 @@
+/*
+MESMA QUANTIDADE DE NÍVEIS PARA AMBAS AS SUBARVORES
+TANTO PARA DIREITA QUANTO PARA ESQUERDA
+
+MAIOR QUE 1 E MENOR QUE -1 = DESEQUILIBRADA
+
+corrigir com rotações:
+
+rotação a esquerda
+rotação a direita
+rotação dupla a esquerda
+rotação dupla a direita
+
+*/
