8af68f35a3ad455c9c3732fe4382ddbce6b11912
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2022-11-14 20:13:59 -0300

    criar novo nó e novo TAD
    Signed-off-by: Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>

--- MATA40/ARVORE /arvoreAVL.c
+++ MATA40/ARVORE /arvoreAVL.c
@@ -11,4 +11,28 @@ rotação a direita
 rotação dupla a esquerda
 rotação dupla a direita
 
+TODO NÓ PRECISA TER UM CAMPO "ALTURA"
+
+typedef struct node{
+  int valor;
+  struct node *left;
+  struct node *right;
+  short altura; //short ocupa apenas 2 bytes de memória
+}Node;
+
+CRIAR UM NOVO NÓ
+
+Node* newNode(int x){
+  Node *new = malloc(sizeof(Node));
+  if(new){
+  new->valor = x;
+  new->left = NULL;
+  new->right = NULL;
+  new->altura = 0;
+} else{
+  printf("\nERRO ao alocar um novo NO!\n");
+  return novo;
+}
+
+
 */
