896484c002e837cb389b6e4c76132c0e27ef987f
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2022-11-15 23:05:02 -0300

    main
    Signed-off-by: Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>

--- MATA40/ARVORE /arvoreAVL[TEORIA].c
+++ MATA40/ARVORE /arvoreAVL[TEORIA].c
@@ -262,8 +262,29 @@ int main(){
   do{
     printf("\n\n\t0 - Sair\n\t1- Inserir\n\t2- Remover\n\t3 - Imprimir\n\n");
     scanf("%d", &opcao);
-  
-  
+    switch(opaco){
+      case 0:
+        printf("Saindo!!!");
+        break;
+       case 1:
+        printf("\tDigite o valor a ser inserido: ");
+        scanf("%d", &valor);
+        raiz = inserir(raiz, valor);
+        break;
+       case 2:
+        printf("\tDigite o valor a ser Removido: ");
+        scanf("%d", &valor);
+        raiz = remover(raiz, valor);
+        break;
+       case 3:
+        imprimir(raiz, 1);
+        break;
+       default:
+       print("\nOpcao Invalida!!!\n");
+    }
+   }while(opcao != 0);
+  return 0;
+ }
   
   
   }
