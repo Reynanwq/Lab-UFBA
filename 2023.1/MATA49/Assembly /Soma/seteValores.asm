50d138020504f1d1548888fb0a945fc4a97a82b1
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-05-08 01:44:01 -0300

    somando sete valores assembler nasm
    Signed-off-by: Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>

---
+++ 2023.1/MATA49/Assembly /Soma/seteValores.asm
@@ -0,0 +1,34 @@
+; SOMANDO SETE VALORES
+
+MOV A, 3
+MOV B, 1
+MOV C, 2
+
+ADD A, B ; 3 + 1 = 4
+MOV D, A 
+
+ADD A, C ; 4 + 2 = 6 
+MOV D, A
+
+MOV A, 1
+MOV B, 2
+MOV C, 3
+
+ADD D, A ; 6 + 1 = 7
+ADD D, B ; 7 + 2 = 9
+ADD D, C ; 9 + 3 = 12
+
+MOV A, D
+MOV B, 4
+MOV C, 6
+
+ADD A, B ; 12 + 9 = 21
+ADD A, C ; 21 + 6 = 27
+MOV D, A 
+
+;A = 27
+;B = 4
+;C = 6
+;D = 27
+
+ 
