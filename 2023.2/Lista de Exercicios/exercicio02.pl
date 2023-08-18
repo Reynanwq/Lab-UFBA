bb90217e07d94f36160f5b0a812c1f264ed065b1
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-08-18 18:04:44 -0300

    prolog add list exercice
    Signed-off-by: Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>

---
+++ 2023.2/Lista de Exercicios/exercicio02.pl
@@ -0,0 +1,11 @@
+profissao(messi, futebolista).
+torcedor(messi, barcelona).
+jogador(messi, psg).
+nacionalidade(messi, argentino).
+posicao(messi, 'meia-atacante').
+
+nacionalidade(rodrigo, brasileiro).
+posicao(rodrigo, atacacnte).
+jogador(rodrigo, 'real madrid').
+% se vai separar, utilize aspas simples
+torcedor(rodrigo, santos).
