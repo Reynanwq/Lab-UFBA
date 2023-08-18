1fbf402ca62c6bb4e322ad43cd666bcaa8844b80
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-08-18 18:08:50 -0300

    Create exercicio04.pl
    Signed-off-by: Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>

---
+++ 2023.2/Lista de Exercicios/exercicio04.pl
@@ -0,0 +1,15 @@
+woman(pam).
+woman(ann).
+woman(pat).
+woman(liz).
+
+man(tom).
+man(bob).
+man(jim).
+
+genitor(tom, bob).
+genitor(tom, liz).
+genitor(pam, bob).
+genitor(bob, ann).
+genitor(bob, pat)
+genitor(pat, jim).
