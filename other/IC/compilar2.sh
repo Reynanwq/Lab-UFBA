51a2cfbdc9cb5f756bd7ea15065123e546ba13f7
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2023-09-20 17:17:24 -0300

    compilar2.sh
    Signed-off-by: Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>

---
+++ other/IC/compilar2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SECONDS=0
+clear
+export PATH=~/Castalia/omnetpp-4.6/bin:$PATH
+export PATH=$PATH:~/Castalia/omnetpp-4.6/bin
+export LD_LIBRARY_PATH=~/Castalia/omnetpp-4.6/lib
+which opp_makemake
+
+make
+
+
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+
+
+
